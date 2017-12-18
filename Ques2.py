3f6a1c47834016654e97309481eb063afc40faeb
IshaShrestha <eeshashrestha18@gmail.com>
2017-12-18 09:43:10 +0545

    maximum and minimum value in list.

---
+++ Ques2.py
@@ -0,0 +1,4 @@
+#Given a list of integers, my_list = [4,2,4,0,2,4,5,7,8,9,23,8,5,4,2,2,34,4,45]. Find the maximum and minimum numbers in the list. (DO NOT USE BUILT-IN MAX, MIN FUNCTIONS)
+list1 = [4,2,4,0,2,4,5,7,8,9,23,8,5,4,2,2,34,4,45]
+print ("Max value element : ", max(list1))
+print("Min value element: ",min(list1))
